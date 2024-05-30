fd7aa64296edb1848c23b1a526be6692a9471958
doeun-kim <doeun2@naver.com>
2024-05-30 00:06:07 -0700

    gp_rdd_plot

--- R/gp_functions.R
+++ R/gp_functions.R
@@ -236,7 +236,16 @@ gp_predict <- function(gp, Xtest){
 #' @param trim a logical value indicating whether you want to do an automatic trim at a specific value of trim_k_value (default=FALSE)
 #' @param trim_k_value a numerical value indicating the kernel value that you want to trim above (default = 0.1)
 #' @param scale a logical value indicating whether you want to scale the covariates (default = TRUE)
-#'
+#' @examples
+#'\dontrun{
+#' n <- 100
+#' tau <- 3
+#' cut <- 0
+#' x <- rnorm(n, 0, 1)
+#' y <- rnorm(n, 0, 1) + tau*ifelse(x>cut, 1, 0)
+#' gp_rdd.out <- gp_rdd(x, y, cut)
+#' gp_rdd_plot(gp_rdd.out)
+#'}
 #' @importFrom stats sd optimize complete.cases qnorm
 #' @importFrom Rcpp sourceCpp
 #' @return \item{tau}{an estimated treatment effect}
@@ -303,4 +312,61 @@ gp_rdd <- function(X, Y, cut, alpha=0.05, b=NULL,
                         trim_at_right=trim_at_right))
   }
   return(results)
+}
+
+#' gp_rdd_plot
+#'
+#' to draw an RD plot using the results obtained from gp_rdd()
+#'
+#' @param gp_rdd_res a list-form results obtained from gp_rdd()
+#' @param l_col a character value indicating the color of the left side of the cutoff point (default = "blue")
+#' @param r_col a character value indicating the color of the right side of the cutoff point (default = "red")
+#' @importFrom ggplot2 ggplot geom_point geom_line geom_ribbon theme_minimal aes
+#' @return \item{gg}{an RD ggplot}
+#' @export
+gp_rdd_plot <- function(gp_rdd_res,
+                        l_col = "blue",
+                        r_col = "red"){
+
+  ggdt <- data.frame(X = gp_rdd_res$X,
+                     Y = gp_rdd_res$Y)
+
+  Xtest_left <- seq(min(ggdt$X[ggdt$X<gp_rdd_res$cut]), gp_rdd_res$cut, length.out=100)
+  gp_pred_l <- gp_predict(gp_rdd_res$gp_train_l, Xtest_left)
+  l_se <- sqrt(diag(gp_pred_l$f_cov_orig))
+
+  Xtest_right <- seq(gp_rdd_res$cut, max(ggdt$X[ggdt$X>gp_rdd_res$cut]), length.out=100)
+  gp_pred_r <- gp_predict(gp_rdd_res$gp_train_r,
+                          Xtest_right)
+  r_se <- sqrt(diag(gp_pred_r$f_cov_orig))
+
+  left_side <- data.frame(x = Xtest_left,
+                          y = gp_pred_l$Ys_mean_orig,
+                          l_lwr = gp_pred_l$Ys_mean_orig - 1.96*l_se,
+                          l_upr = gp_pred_l$Ys_mean_orig + 1.96*l_se
+  )
+
+  right_side <- data.frame(x = Xtest_right,
+                           y = gp_pred_r$Ys_mean_orig,
+                           r_lwr = gp_pred_r$Ys_mean_orig - 1.96*r_se,
+                           r_upr = gp_pred_r$Ys_mean_orig + 1.96*r_se
+  )
+
+  gg <- ggplot2::ggplot() +
+    ggplot2::geom_point(data = ggdt,
+                        ggplot2::aes(X, Y), alpha = 0.5) +
+    ggplot2::geom_line(data = left_side,
+                       ggplot2::aes(x, y), col = l_col) +
+    ggplot2::geom_ribbon(data = left_side,
+                         ggplot2::aes(x, ymin = l_lwr, ymax = l_upr),
+                         col = NA, alpha = 0.3, fill = l_col) +
+    ggplot2::geom_line(data = right_side,
+                       ggplot2::aes(x, y),
+                       col = r_col) +
+    ggplot2::geom_ribbon(data = right_side,
+                         ggplot2::aes(x, ymin = r_lwr, ymax = r_upr),
+                         col = NA, alpha = 0.3, fill = r_col) +
+    ggplot2::theme_minimal()
+
+  return(gg)
 }
