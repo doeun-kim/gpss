f300de6009c8ba3829e3314efb3948d304990d60
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2025-01-18 14:30:30 -0500

    predict in rvar format

--- R/predict.R
+++ R/predict.R
@@ -1,10 +1,25 @@
 #' predict method for `gpss` objects
 #'
+#' @param newdata data frame on which to make predictions (test set)
+#' @param type "response" or "scaled"
+#' @param format "default" or "rvar"
+#' @inheritParams stats::predict
+#'
 #' @export
-predict.gpss <- function(object, newdata = NULL, type = "response") {
+predict.gpss <- function(object, newdata = NULL, type = "response", format = "default") {
+  if (!isTRUE(format %in% c("default", "rvar"))) {
+    msg <- '`format` must be "default" or "rvar".'
+    stop(msg, call. = FALSE)
+  }
+
   type <- match.arg(type, c("response", "scaled"))
   if (!inherits(newdata, "data.frame")) {
     msg <- "`newdata` must be a data frame."
+    stop(msg, call. = FALSE)
+  }
+
+  if (!identical(type, "response") && identical(format, "rvar")) {
+    msg <- '`format="rvar"` is only available with `type="response"`.'
     stop(msg, call. = FALSE)
   }
 
@@ -13,6 +28,8 @@ predict.gpss <- function(object, newdata = NULL, type = "response") {
     msg <- "The `gpss` object must have been created using the formula interface of the `gpss()` function."
     stop(msg, call. = FALSE)
   }
+  # Drop the left-hand side
+  fo <- as.formula(paste("~", deparse(formula(fo)[[3]])))
 
   X <- model.matrix(fo, newdata)
 
@@ -26,7 +43,22 @@ predict.gpss <- function(object, newdata = NULL, type = "response") {
   out <- gp_predict(object, Xtest = X)
 
   if (type == "response") {
-    out <- out$Ys_mean_orig
+    if (format == "rvar") {
+      if (!requireNamespace("posterior")) {
+        msg <- "Please install the `posterior` package."
+        stop(msg, call. = FALSE)
+      }
+      # se <- sqrt(diag(gp_pred_l$f_cov_orig)[1] + diag(gp_pred_r$f_cov_orig)[1])
+      mu <- out$Ys_mean_orig
+      se <- sqrt(diag(out$f_cov_orig))
+      rv <- lapply(seq_along(mu), function(i) rnorm(1e4, mu[i], se[i]))
+      rv <- do.call(cbind, rv)
+      rv <- posterior::rvar(rv)
+      out <- newdata
+      out$rvar <- posterior::rvar(rv)
+    } else {
+      out <- out$Ys_mean_orig
+    }
   } else if (type == "scaled") {
     out <- out$Ys_mean_scaled
   }
